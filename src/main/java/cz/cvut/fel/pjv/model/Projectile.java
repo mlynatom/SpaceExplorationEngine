c4878e3e23ff74993287410f6391c471fd2e1b1d
Thomasprogramer <tom.mlynar1@gmail.com>
2021-03-29 21:47:33 +0200

    problem of 2 damage parameters is ship and projectile classes

--- src/main/java/cz/cvut/fel/pjv/model/Projectile.java
+++ src/main/java/cz/cvut/fel/pjv/model/Projectile.java
@@ -6,12 +6,10 @@ import javafx.scene.image.Image;
  * Projectile class for objects which can be shot from ship with entered damage and lifespan. It is based on Actor class.
  */
 public class Projectile extends Actor {
-	private double damage;
 	private double lifeSpan;
 
-	public Projectile(double iX, double iY, String spriteBound, double damage, double lifeSpan, Image... spriteImage) {
+	public Projectile(double iX, double iY, String spriteBound, double lifeSpan, Image... spriteImage) {
 		super(iX, iY, spriteBound, spriteImage);
-		this.damage = damage;
 		this.lifeSpan = lifeSpan;
 	}
 
